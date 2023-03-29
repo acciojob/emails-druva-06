0cf8d0efd346c8d4eb0796df4cc31cb559ff9d75
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-30 02:02:17 +0530

    Added to Trash

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -23,7 +23,10 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-        if(inbox.size() == inboxCapacity) inbox.remove(0);
+        if(inbox.size() == inboxCapacity){
+            trash.add(inbox.get(0));
+            inbox.remove(0);
+        }
         inbox.add(Triple.of(date,sender,message));
     }
 
